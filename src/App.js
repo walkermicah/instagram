409c5efc745b66765e348d363225bc1f9e23667e
Walker <walker.m.oliver@gmail.com>
2023-04-06 11:48:48 -0400

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"></div>;
-}
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      {/* TO DO: login path */}
+      {/* TO DO: home redirects to login if signed out */}
+      {/* TO DO: login redirects to home if signed in */}
+    </>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
