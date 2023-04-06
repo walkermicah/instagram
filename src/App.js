1a994ee054630c664b447bb672a1631761f66b2e
Walker <walker.m.oliver@gmail.com>
2023-04-06 16:18:35 -0400

    Edit nested route structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {
   Route,
 } from 'react-router-dom';
 
-import Root from './layouts/Root';
+import RootLayout from './layouts/RootLayout';
+import AppLayout from './layouts/AppLayout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -13,8 +14,10 @@ import SignUp from './pages/SignUp';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Root />}>
-        <Route index element={<Home />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route element={<AppLayout />}>
+          <Route index element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Route>
