11e17283c2ba81f5da889d091d57e5db2e9459ea
Walker <walker.m.oliver@gmail.com>
2023-04-12 14:19:46 -0400

    Format and style page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,57 @@
+import { Form, Link } from 'react-router-dom';
+import facebook from '../../img/facebook-blue.png';
 import styles from './Login.module.scss';
 
 const Login = () => {
-  // const container = styles.container;
+  const container = styles.container;
+  const section = styles.section;
+  const sectionPrimary = styles.sectionPrimary;
+  const sectionSecondary = styles.sectionSecondary;
+  const formControls = styles.formControls;
+  const checkbox = styles.checkbox;
+  const divider = styles.divider;
+  const fbLoginBtn = styles.fbLoginBtn;
+
   return (
-    <article>
-      <h1>Instagram</h1>
+    <article className={container}>
+      <div className={`${section} ${sectionPrimary}`}>
+        <h1>Instagram</h1>
+        <Form>
+          <div className={formControls}>
+            <label htmlfor="username">
+              <input
+                type="text"
+                name="username"
+                placeholder="Username or email"
+              />
+              <span>Username or email</span>
+            </label>
+            <label htmlfor="password">
+              <input type="password" name="password" placeholder="Password" />
+              <span>Password</span>
+            </label>
+          </div>
+
+          <label className={checkbox}>
+            <input type="checkbox" name="saveLogin" />
+            Save login info
+          </label>
+          <button disabled>Log in</button>
+        </Form>
+        <div className={divider}>
+          <span></span>
+          OR
+          <span></span>
+        </div>
+        <button className={fbLoginBtn}>
+          <img src={facebook} alt="" />
+          Log in with Facebook
+        </button>
+      </div>
+
+      <div className={`${section} ${sectionSecondary}`}>
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </div>
     </article>
   );
 };
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,5 +1,62 @@
+import { Form, Link } from 'react-router-dom';
+import facebook from '../../img/facebook-white.png';
+import styles from './SignUp.module.scss';
+
 const SignUp = () => {
-  return <div className="login-container"></div>;
+  const container = styles.container;
+  const section = styles.section;
+  const sectionPrimary = styles.sectionPrimary;
+  const formControls = styles.formControls;
+  const divider = styles.divider;
+  const fbLoginBtn = styles.fbLoginBtn;
+  const sectionSecondary = styles.sectionSecondary;
+
+  return (
+    <article className={container}>
+      <div className={`${section} ${sectionPrimary}`}>
+        <h1>Instagram</h1>
+        <h2>
+          <span>Sign up to see photos and videos</span>
+          <span>from your friends.</span>
+        </h2>
+        <button className={fbLoginBtn}>
+          <img src={facebook} alt="" />
+          Log in with Facebook
+        </button>
+        <div className={divider}>
+          <span></span>
+          OR
+          <span></span>
+        </div>
+        <Form>
+          <div className={formControls}>
+            <label htmlfor="email">
+              <input type="email" name="email" placeholder="Email" />
+              <span>Email</span>
+            </label>
+            <label htmlfor="fullname">
+              <input type="text" name="fullname" placeholder="Full name" />
+              <span>Full name</span>
+            </label>
+            <label htmlfor="username">
+              <input type="text" name="username" placeholder="Username" />
+              <span>Username</span>
+            </label>
+            <label htmlfor="password">
+              <input type="password" name="password" placeholder="Password" />
+              <span>Password</span>
+            </label>
+          </div>
+
+          <button disabled>Sign up</button>
+        </Form>
+      </div>
+
+      <div className={`${section} ${sectionSecondary}`}>
+        Have an account? <Link to="/login">Log in</Link>
+      </div>
+    </article>
+  );
 };
 
 export default SignUp;
